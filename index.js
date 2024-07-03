527c55cc7a713d6dec494f369fc5ff29371f35e9
Namima Islam <namimaislam308@gmail.com>
2024-07-03 18:14:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+//superbowlWin(record)
+//returns a year the Denver Broncos won the superbowl
+//returns undefined if the record has no win objects
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(record) {
+   
+    const winRecord = record.find(item => item.result === 'W');
+    
+   
+    return winRecord ? winRecord.year : undefined;
+}
+
+console.log(superbowlWin(record));
